48a7719a31efc6e944d5e61c17e1e5dec71e744d
danielkmariam <danielkmariam@gmail.com>
2015-12-10 10:46:50 +0000

    Updat webpack config: common, target, build

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
+var merge = require('webpack-merge'); 
 
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
 	app: path.join(__dirname, 'app'),
 	build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+var common = {
 	entry: PATHS.app,
  	output: {
    	path: PATHS.build,
@@ -27,19 +29,33 @@ module.exports = {
  			}
  		]
  	},
-	devServer: {
-		historyApiFallback : true,
-		hot: true,
-		inline: true,
-		progress: true,
-		status: 'errors-only',
-		host: process.env.HOST,
-		port: process.env.PORT
-	},
 	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
 		new HtmlWebpackPlugin({
 			title: 'Kanban app'
 		})
 	]
 };
+
+if (TARGET === 'start' || !TARGET) {
+	module.exports = merge(common, {
+		devtool: 'eval-source-map',
+		devServer: {
+			historyApiFallback : true,
+			hot: true,
+			inline: true,
+			progress: true,
+			status: 'errors-only',
+			host: process.env.HOST,
+			port: process.env.PORT
+		},
+		plugins: [
+			new webpack.HotModuleReplacementPlugin()
+		]
+	});
+}
+
+if (TARGET === 'build') {
+	module.exports = merge(common, {
+		devtool: 'source-map'
+	});
+}
